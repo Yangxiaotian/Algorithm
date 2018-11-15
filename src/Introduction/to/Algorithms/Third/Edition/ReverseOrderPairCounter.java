5ae2522811bf77d4ce64adf05a6e704136511416
chuanshi <chuanshi@192.168.31.201>
2018-11-15 21:29:48 +0800

    complete reverse order pair counter

--- src/Introduction/to/Algorithms/Third/Edition/ReverseOrderPairCounter.java
+++ src/Introduction/to/Algorithms/Third/Edition/ReverseOrderPairCounter.java
@@ -1,7 +1,7 @@
 package Introduction.to.Algorithms.Third.Edition;
 
 public class ReverseOrderPairCounter {
-	public static void merge(int[] A, int p, int q, int r) {
+	public static int merge(int[] A, int p, int q, int r) {
 		int n1 = q - p + 1;
 		int n2 = r - q;
 		int[] L = new int[n1];
@@ -11,25 +11,59 @@ public class ReverseOrderPairCounter {
 		for(j = 0; j < n2; j++) R[j] = A[q+j+1];
 		int k = p;
 		for(i = 0, j = 0; i < n1 && j < n2; ) {
-			if(L[i] < R[j]) {
+			if(L[i] <= R[j]) {
 				A[k++] = L[i++];
 			}else {
 				A[k++] = R[j++];
 			}
 		}
+		int counter = j*(n1-i);
 		while(i < n1) {
 			A[k++] = L[i++];
 		}
 		while(j < n2) {
 			A[k++] = R[j++];
 		}
+		return counter;
 	}
-	public static void mergeSort(int[] A, int p, int r) {
+	public static int mergeSort(int[] A, int p, int r) {
+		int sum = 0;
 		if(p < r) {
 			int q = (p+r)/2;
-			mergeSort(A, p, q);
-			mergeSort(A, q+1, r);
-			merge(A, p, q, r);
+			sum += mergeSort(A, p, q);
+			sum += mergeSort(A, q+1, r);
+			sum += merge(A, p, q, r);
 		}
+		return sum;
 	} 
+	
+	public static int insertSort(int[] A) {
+		int n = A.length;
+		int counter = 0;
+		for(int i = 1; i < n; i++) {
+			int key = A[i];
+			int j = i - 1;
+			while(j >= 0 && key < A[j]) {
+				A[j+1] = A[j];
+				j--;
+				counter++;
+			}
+			A[j+1] = key;
+		}
+		return counter;
+	}
+	public static void main(String[] args) {
+		int n = 100000;
+		int[] A = new int[n];
+		for(int i = 0; i < n; i++) {
+			A[i] = n - i;
+		}
+		long t1 = System.currentTimeMillis();
+		System.out.println(mergeSort(A, 0, n-1));
+//		System.out.println(insertSort(A));
+		long t2 = System.currentTimeMillis();
+		System.out.println("cost:"+(t2-t1)/1000.0+"s");
+		
+		
+	}
 }

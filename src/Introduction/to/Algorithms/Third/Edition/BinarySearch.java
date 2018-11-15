2a7f81081c923649d598383595f224c1050fd024
chuanshi <chuanshi@192.168.31.201>
2018-11-15 21:43:24 +0800

    add binarySearchLoop

--- src/Introduction/to/Algorithms/Third/Edition/BinarySearch.java
+++ src/Introduction/to/Algorithms/Third/Edition/BinarySearch.java
@@ -11,12 +11,25 @@ public class BinarySearch {
 			return -1;
 		}
 	}
+	/**
+	 * 不用递归的二叉查找
+	 * @param args
+	 */
+	public static int binarySearchLoop(int[] A, int key, int p, int r) {
+		while(p <= r) {
+			int q = (p + r)/2;
+			if(key == A[q]) return q;
+			else if(key < A[q]) r = q -1;
+			else p = q + 1;
+		}
+		return -1;
+	}
 	public static void main(String[] args) {
 //		int[] A = new int[50];
 //		for(int i = 0; i < 50; i++)
 //			A[i] = 50-i;
 //		MergeSort.mergeSort(A, 0, 49);
 		int[] A = {1, 3, 4, 6};
-		System.out.println(binarySearch(A, 2, 0, 3));
+		System.out.println(binarySearchLoop(A, 4, 0, 3));
 	}
 }
